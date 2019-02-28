b48c39069f17d171c3607b321e2e5b0837af668c
jjgorton <jjgorton@gmail.com>
2019-02-28 15:48:39 -0500

    completed all 4 constructors, ready to build objects to use them

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,3 +26,35 @@ class Instructor extends Person {
         console.log(`${student.name} receives a perfect score on ${subject}`);
     }
 }
+
+class Student extends Person {
+    constructor(learn) {
+        super(learn);
+        this.previousBackground = learn.previousBackground;
+        this.className = learn.className;
+        this.favSubjects = learn.favSubjects;
+    }
+    listsSubjects(list) {
+        console.log(list);
+    }
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
+}
+
+class ProjectManagers extends Instructor {
+    constructor(help) {
+        super(help);
+        this.gradClassName = help.gradClassName;
+        this.favInstructor = help.favInstructor;
+    }
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`);
+    }
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
